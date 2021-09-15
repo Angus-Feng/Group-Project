8794aa7e8a209b308f68bc247457894ffc85269f
nariroh <nari.rohh@gmail.com>
2021-09-15 15:00:02 -0400

    feat: create bakeryItems array

---
+++ scripts/shop.js
@@ -0,0 +1,57 @@
+const bakeryItems = [
+    {
+        id: 0,
+        type: "cakes",
+        imgUrl: "../images/blackberry-cake.jpeg",
+        title: "vanila black berry",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum numquam magni sed voluptas delenitiomnis quod amet. Enim cum blanditiis eligendi numquam ullam dolorem accusantium optio vitae perferendis iste. Dolorum.",
+        portion: "4-6",
+        price: 40
+    },
+    {
+        id: 1,
+        type: "cakes",
+        imgUrl: "../images/rasberry.jpeg",
+        title: "berry festival",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum numquam magni sed voluptas delenitiomnis quod amet. Enim cum blanditiis eligendi numquam ullam dolorem accusantium optio vitae perferendis iste. Dolorum.",
+        portion: "3-4",
+        price: 30
+    },
+    {
+        id: 2,
+        type: "cakes",
+        imgUrl: "../images/chocolate-moka-cream.jpeg",
+        title: "chocolate moka cream",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum numquam magni sed voluptas delenitiomnis quod amet. Enim cum blanditiis eligendi numquam ullam dolorem accusantium optio vitae perferendis iste. Dolorum.",
+        portion: "4-6",
+        price: 40
+    },
+    {
+        id: 3,
+        type: "cakes",
+        imgUrl: "../images/oreo-chocolate.jpeg",
+        title: "oreo chocolate",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum numquam magni sed voluptas delenitiomnis quod amet. Enim cum blanditiis eligendi numquam ullam dolorem accusantium optio vitae perferendis iste. Dolorum.",
+        portion: "3-4",
+        price: 30
+    },
+    {
+        id: 4,
+        type: "cakes",
+        imgUrl: "../images/blueberry-cream.jpeg",
+        title: "blue berry walnut cream",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum numquam magni sed voluptas delenitiomnis quod amet. Enim cum blanditiis eligendi numquam ullam dolorem accusantium optio vitae perferendis iste. Dolorum.",
+        portion: "3-4",
+        price: 35
+    },
+    {
+        id: 5,
+        type: "cakes",
+        imgUrl: "../images/cocoa-cheese.jpeg",
+        title: "berry festival",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum numquam magni sed voluptas delenitiomnis quod amet. Enim cum blanditiis eligendi numquam ullam dolorem accusantium optio vitae perferendis iste. Dolorum.",
+        portion: "4-6",
+        price: 40
+    },
+]
+
