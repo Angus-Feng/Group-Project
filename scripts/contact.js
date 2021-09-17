b0cad43e65b1dac6c43b6a1d8698071b5964880a
YuanYuan <yuanyuanceline@gmail.com>
2021-09-16 21:16:02 -0400

    feat: contact us form validation

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,4 +1,4 @@
-let messages = [];
+/*let messages = [];
 const addMessage = (ev) => {
     ev.preventDefault(); // to stop the form submitting
     let topic = document.getElementById('topic');
@@ -17,4 +17,59 @@ const addMessage = (ev) => {
     
     // saving to locatstorage
     localStorage.setItem('MessageList', JSON.stringify(messages));
-}
+}*/
+
+function validateForm() {
+    // decalre variables
+    let fullNameText = document.getElementById('input-full-name');
+    let emailField = document.getElementById('input-email');
+    let emailText = document.getElementById('input-email').value;
+    let msgTopic = document.getElementById('topic');
+    let msgContent = document.getElementById('feedback');
+    let emailCorrectPattern = /^[\w\-\.\+]+\@[a-zA-Z0-9\. \-]+\.[a-zA-z0-9]{2,4}$/;
+
+    // validate Name
+    if (fullNameText.value.length === 0) {
+        alert('Please enter your full name');
+        fullNameText.focus();
+        fullNameText.style.background = 'yellow';
+        return false;
+    } // end if
+    else if (emailText.length === 0) { // validate email 
+        alert('Please enter your email address');
+        emailField.focus();
+        emailField.style.background = 'yellow';
+        fullNameText.style.background = 'white';
+        return false;
+    }// end if    
+    else if (!(emailText.match(emailCorrectPattern))) { // validate email pattern
+        alert('Please correct your email address');
+        // emailField.value = '';
+        emailField.focus();
+        emailField.style.background = 'yellow';
+        fullNameText.style.background = 'white';
+        return false;
+    }// end if
+    else if (msgTopic.selectedIndex === 0) {  // validate topic
+        alert('Please choose an option');
+        msgTopic.focus();
+        msgTopic.style.background = 'yellow';
+        fullNameText.style.background = 'white';
+        emailField.style.background = 'white';
+        return false;
+    }// end if     
+    else if (msgContent.value.length === 0) {  // validate message
+        alert('Please enter your message');
+        msgContent.focus();
+        msgContent.style.background = 'yellow';
+        fullNameText.style.background = 'white';
+        emailField.style.background = 'white';
+        msgTopic.style.background = 'white';
+        return false;
+    }// end if 
+    else {
+        alert('Thank you for your message. We will reply you in two business days.');
+        return true;
+    }
+    msgContent.style.background = 'white';
+}// end function validateForm()
